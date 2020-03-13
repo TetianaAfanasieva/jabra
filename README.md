abdaa6b963102cfbd933d1079ccc7c8c5e020cb7
Tetiana Afanasieva <t.afanasieva@dreamteam.gg>
2020-03-13 14:05:41 +0200

    fourth  commit

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Tetiana Afanasieva
 
 Install dependencies with `npm install` or `npm ci`
 
-See scripts in `package.json` to start the local API server and run the tests. The main ones are
+See scripts in `package.json` to run the tests. The main ones are
 
 * `npm run cy:open` - runs Cypress in GUI mode
-* `npm test` - starts the local API server and runs Cypress tests against it in headless mode
+* `npm test` - runs Cypress tests against it in headless mode
--- cypress/integration/Parrot_E2E_spec.js
+++ cypress/integration/Parrot_E2E_spec.js
@@ -3,8 +3,9 @@ describe('add Parrot S450-XT to cart ', () => {
    it('should add product to cart from Home Page', () => {
         cy.visit('https://www.blueparrott.com/');
         cy.get('title').should('have.text', 'Wireless Headsets | On the go headsets | BlueParrott');
-        cy.get('[data-slick-index="3"]').click()
-        cy.get('[ng-click="execute()"]').click()
+        cy.get('[data-slick-index="3"]').click();
+        cy.get('[ng-click="execute()"]').click();
+        cy.wait(1000);
         cy.url().should('include', 'checkout.blueparrott.com/store');
     });
 
